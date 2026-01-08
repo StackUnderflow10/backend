58ecc3e44f71a649af450aeaabe88a542a8fd4de
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2026-01-08 11:28:02 +0530

    removed 'app/config.py' since it wasnt being used and moved its code in 'app/firebase_init.py'

--- app/firebase_init.py
+++ app/firebase_init.py
@@ -4,9 +4,22 @@ import os
 import firebase_admin
 from firebase_admin import credentials, firestore
 import pyrebase
-from .config import firebaseConfig
+from dotenv import load_dotenv
+
+load_dotenv()
 
 IS_CI = os.environ.get("CI") == "true"
+
+firebaseConfig = {
+    "apiKey": os.getenv("FIREBASE_API_KEY"),
+    "authDomain": os.getenv("FIREBASE_AUTH_DOMAIN"),
+    "databaseURL": os.getenv("FIREBASE_DATABASE_URL"),
+    "projectId": os.getenv("FIREBASE_PROJECT_ID"),
+    "storageBucket": os.getenv("FIREBASE_STORAGE_BUCKET"),
+    "messagingSenderId": os.getenv("FIREBASE_MESSAGING_SENDER_ID"),
+    "appId": os.getenv("FIREBASE_APP_ID"),
+    "measurementId": os.getenv("FIREBASE_MEASUREMENT_ID")
+}
 
 firebase = pyrebase.initialize_app(firebaseConfig)
 
